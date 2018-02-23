7ce326bef228347bef9c1a36a3f03df7f4ade4d1
Rodrigo da Silva Moura <rodrigo.browning@gmail.com>
2018-02-23 13:49:59 -0300

    Adding and remove functions

--- app.js
+++ app.js
@@ -17,13 +17,23 @@ console.log('command', process.argv);
 console.log('Command: ', yargs.argv);
 	
 	if(command == 'add'){
-		notes.addNote(argv.title, argv.body);
+		let note = notes.addNote(argv.title, argv.body);
+		if(note){
+			console.log('note created');
+			console.log('---');
+			console.log(`Title: ${note.title}`);
+			console.log(`Body: ${note.body}`);	
+		}else{console.log('Note title taken')}
+		
+
 	}else if(command == 'list'){
 		console.log("listing all notes");
 	}else if(command == 'read'){
 		console.log('Reading notes');
 	}else if(command == 'remove'){
-		console.log('Removing note');
+		let bool = notes.removeNote(argv.title);
+		let msg  = bool ? "item deleted" : "item not found";
+		console.log(msg);
 	}else{
 		console.log('command not recognized');
 	}
--- notes.js
+++ notes.js
@@ -1,29 +1,54 @@
 console.log('starting notes.js');
 const fs = require('fs');
 
-let addNote = (title, body) => {
-	let notes = [];
+var readFile = () => {	
+	try{
+		let notes_file = fs.readFileSync('notes-data.json');
+		return JSON.parse(notes_file);			
+	}catch(e){
+		return [];
+	}
+};
+
+var saveNotes = (notes) => {
+	fs.writeFileSync('notes-data.json', JSON.stringify(notes));
+};
+
+var addNote = (title, body) => {
+	let notes = readFile();
 	let note = {
 		title,
 		body
 	};
 
-	try{
-		notes = JSON.parse(fs.readFileSync('notes-data.json'));	
-		
-	}catch(e){}
 	
 	
+	let duplicacao = notes.filter((note) => {return note.title === title});
 
-	let duplicacao = notes.filter((note) => {note.title == title});
-
-	if(duplicacao.length > 0){
+	
+	if(duplicacao.length === 0){
 		notes.push(note);	
-		fs.writeFileSync('notes-data.json', JSON.stringify(notes));	
+		saveNotes(notes);
+		return note;
 	}
 	
 };
 
+var removeNote = (title) => {
+	//Removing note
+
+		//fetch data
+		let notes = readFile();
+		//filter array
+		let newNotes = notes.filter((note) => {return note.title !== title});
+		//seve new array
+		saveNotes(newNotes);
+		return notes.length != newNotes.length
+			
+		
+};
+
 module.exports = {
-	addNote
+	addNote,
+	removeNote
 };
